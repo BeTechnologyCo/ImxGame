b75b7412930beaa9a7dc7b6326510971d000cab9
eddyb <eddyboughioul@hotmail.fr>
2023-02-08 18:06:26 +0100

    init repo

---
+++ Assets/Service/AreaService.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
+//using Debug = UnityEngine.Debug;
+
+//namespace Assets.Service
+//{
+//    public class AreaService : BaseService
+//    {
+//        Dictionary<Guid, Stopwatch> dicoTime = new Dictionary<Guid, Stopwatch>();
+
+//        public event EventHandler<List<AreaDto>> AreasLoaded;
+//        public event EventHandler<string> AreaError;
+//        public override string HubName => ServerConstants.areaHub;
+//        public AreaService()
+//        {
+//        }
+
+//        public async Task GetAreas(PlayerDto hero)
+//        {
+//            Debug.Log("GetAreas");
+//            var guid = Guid.NewGuid();
+//            Stopwatch sw = new Stopwatch();
+//            sw.Start();
+//            dicoTime.Add(guid, sw);
+//            await connection.SendAsync("GetArea", guid, hero);
+//            return;
+//        }
+
+//        public async override Task Start()
+//        {
+//            await connection.ConnectAsync();
+//            //connection.ServerTimeout = TimeSpan.FromSeconds(10);
+
+//            connection.On<PlayerAreasDto>("AreaLoaded", (result) =>
+//            {
+//                Debug.Log("AreaLoaded");
+//                var sw = dicoTime[result.CallId];
+//                sw.Stop();
+//                UnityEngine.Debug.Log($"Get AreaHub in {sw.ElapsedMilliseconds} ms");
+//                dicoTime.Remove(result.CallId);
+//                if (AreasLoaded != null)
+//                {
+//                    AreasLoaded(this, result.Areas);
+//                }
+//            });
+
+//            connection.On<string>("AreaError", (result) =>
+//            {
+//                Debug.Log("AreaError");
+//                if (AreaError != null)
+//                {
+//                    AreaError(this, result);
+//                }
+//            });
+//        }
+//    }
+//}
---
+++ Assets/Service/BaseService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class BaseService : IDisposable
+{
+
+    protected string url = "https://hero.opm.one";
+    protected HubConnection connection;
+
+    public abstract string HubName { get; }
+
+    public event EventHandler<string> ServiceError;
+
+    static bool serializerRegistered = false;
+
+    public BaseService()
+    {
+        url = "http://127.0.0.1:54244";
+
+#if UNITY_EDITOR
+        url = "http://127.0.0.1:54244";
+#endif
+
+        try
+        {
+            connection = new HubConnectionBuilder()
+                  .WithUrl($"{url}/{HubName}", options =>
+                  {
+                      options.AccessTokenProvider = () =>
+                      {
+                          //Debug.Log("Token : " + GameContext.Instance.Token);
+                          return Task.FromResult(GameContext.Instance.Token);
+                      };
+                  })
+                  .WithAutomaticReconnect()
+                  .Build();
+
+            Start();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+            if (ServiceError != null)
+            {
+                ServiceError(this, ex.Message);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Start the connection call after initialize the connection
+    /// </summary>
+    /// <returns></returns>
+    protected abstract Task Start();
+
+    public async virtual void Dispose()
+    {
+        if (connection != null && connection.State != HubConnectionState.Disconnected)
+        {
+            await connection.StopAsync();
+        }
+    }
+}
